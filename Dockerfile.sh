ab3e33ed90b14c26dca4cae85536f356d6a8be25
Anton Belyaev <xantbel@gmail.com>
2016-08-25 19:39:11 +0300

    MSPF-66: correct commit_number meta name

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -8,7 +8,7 @@ MAINTAINER Anton Belyaev <a.belyaev@rbkmoney.com>
 LABEL com.rbkmoney.$SERVICE_NAME.parent=$BASE_IMAGE_NAME \
       com.rbkmoney.$SERVICE_NAME.parent_tag=$BASE_IMAGE_TAG \
       com.rbkmoney.$SERVICE_NAME.commit_id=$(git rev-parse HEAD) \
-      com.rbkmoney.$SERVICE_NAME.commit_num=$(git rev-list --count HEAD) \
+      com.rbkmoney.$SERVICE_NAME.commit_number=$(git rev-list --count HEAD) \
       com.rbkmoney.$SERVICE_NAME.branch=$( \
         if [ "HEAD" != $(git rev-parse --abbrev-ref HEAD) ]; then \
           echo $(git rev-parse --abbrev-ref HEAD); \
